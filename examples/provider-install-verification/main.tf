d671a4719b39de31c6044e2af6831e0f6bb3ac59
Suyash Sonawane <sonawsuyash@proton.me>
2026-01-16 15:07:49 +0100

    feat: creates hrobot provider which satisfies the provider interface

---
+++ examples/provider-install-verification/main.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_providers {
+    hrobot = {
+      source = "registry.terraform.io/suyash-811/hrobot"
+    }
+  }
+}
+
+provider "hrobot" {}
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/hashicorp/terraform-provider-scaffolding-framework
+module terraform-provider-hrobot
 
 go 1.25.5
 

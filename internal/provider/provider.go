6cde0818b90297a1d3fcdd362950cc47748c866a
Suyash S <sonawsuyash@proton.me>
2026-01-18 00:52:36 +0100

    feat: configures provider

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -2,17 +2,28 @@ package provider
 
 import (
 	"context"
+	"terraform-provider-hrobot/internal/client"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 )
+
+const defaultURL string = "https://robot-ws.your-server.de"
 
 // Ensure the implementation satisfies the expected interfaces.
 var (
 	_ provider.Provider = &hrobotProvider{}
 )
+
+type hrobotProviderModel struct {
+	BaseURL  types.String `tfsdk:"url"`
+	Username types.String `tfsdk:"username"`
+	Password types.String `tfsdk:"password"`
+}
 
 // New is a helper function to simplify provider server and testing implementation.
 func New(version string) func() provider.Provider {
@@ -39,11 +50,71 @@ func (p *hrobotProvider) Metadata(_ context.Context, _ provider.MetadataRequest,
 
 // Schema defines the provider-level schema for configuration data.
 func (p *hrobotProvider) Schema(_ context.Context, _ provider.SchemaRequest, resp *provider.SchemaResponse) {
-	resp.Schema = schema.Schema{}
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"url": schema.StringAttribute{
+				Optional: true,
+			},
+			"username": schema.StringAttribute{
+				Required: true,
+			},
+			"password": schema.StringAttribute{
+				Required:  true,
+				Sensitive: true,
+			},
+		},
+	}
 }
 
 // Configure prepares a hrobot API client for data sources and resources.
 func (p *hrobotProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	var config hrobotProviderModel
+	diags := req.Config.Get(ctx, &config)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var baseURL, username, password string
+	if !config.BaseURL.IsNull() {
+		baseURL = config.BaseURL.ValueString()
+	}
+	if !config.Username.IsNull() {
+		username = config.Username.ValueString()
+	}
+	if !config.Password.IsNull() {
+		password = config.Password.ValueString()
+	}
+
+	if baseURL == "" {
+		baseURL = defaultURL
+	}
+
+	if username == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("username"),
+			"Missing Hetzner Robot username",
+			"The provider cannot create the Hetzner Robot HTTP client as there is a missing or empty value for the Robot webservice username. "+
+				"Set the username value in the configuration. ",
+		)
+	}
+
+	if password == "" {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("password"),
+			"Missing Hetzner Robot password",
+			"The provider cannot create the Hetzner Robot HTTP client as there is a missing or empty value for the Robot webservice password. "+
+				"Set the password value in the configuration. ",
+		)
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	c := client.NewHetznerRobotClient(baseURL, username, password)
+	resp.DataSourceData = c
+	resp.ResourceData = c
 }
 
 // DataSources defines the data sources implemented in the provider.
